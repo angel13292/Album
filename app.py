2f298fbcef9243575283be913af34cd645f4e6d6
EC2 Default User <ec2-user@ip-172-31-29-239.eu-central-1.compute.internal>
2016-05-11 16:02:17 +0000

    -my first repo

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
+import boto3
 import os
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
 
 @app.route("/hello")
@@ -9,15 +10,28 @@ def hello():
 @app.route("/")
 def index():
   return render_template('upload_form.html', uploadButtonName="send")
+app.debug = True
+
 
 @app.route("/upload", methods=['POST'])
 def upload():
   files = request.files
   for f in files.getlist('file'):
-    filename = f.filename
-    updir = '/home/ec2-user/s3-2/upload'
-    f.save(os.path.join(updir, filename))
+    #filename = f.filename
+    #updir = '/home/ec2-user/upload-web-interface/upload'
+    #f.save(os.path.join(updir, filename))
+    uploadS3(f)
   return jsonify()
+
+
+
+def uploadS3(file):
+  bucket_name='166665-angel'
+  s3 = boto3.resource('s3')
+  bucket = s3.Bucket(bucket_name)
+  bucket.put_object(Key=file.filename, Body=file)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
+
